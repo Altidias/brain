d579c8f18f455b3e189ba1808ce55819a24a289a
Altidias <jacobbarany@gmail.com>
2020-04-18 00:40:34 +1000

    Update main
    moved keypress functions to seperate files

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <Windows.h>
+#include "keysends.h"
 using namespace std;
-
 
 class neuron {
 public:
@@ -11,31 +11,8 @@ public:
 class nlist {
 public:
 	int neuronarray[10];
-	
+
 };
-
-void doAKeyPress(){
-	// This structure will be used to create the keyboard
-   // input event.
-	INPUT ip;
-
-	Sleep(100);
-
-	// Set up a generic keyboard event.
-	ip.type = INPUT_KEYBOARD;
-	ip.ki.wScan = 0; // hardware scan code for key
-	ip.ki.time = 0;
-	ip.ki.dwExtraInfo = 0;
-
-	// Press the "A" key
-	ip.ki.wVk = 0x41; // virtual-key code for the "a" key
-	ip.ki.dwFlags = 0; // 0 for key press
-	SendInput(1, &ip, sizeof(INPUT));
-
-	// Release the "A" key
-	ip.ki.dwFlags = KEYEVENTF_KEYUP; // KEYEVENTF_KEYUP for key release
-	SendInput(1, &ip, sizeof(INPUT));
-}
 
 
 int main() {
